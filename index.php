a7aeeecd7c0eef7d86c4bc059a22e27e1e5db671
Boris <suhaimi7216@yahoo.com>
2019-01-04 11:32:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo 'EMILIA';
+$link = mysql_connect('113.23.167.13', 'Suhaimi', 'pkpp@2019');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo ' Connected successfully';
+mysql_close($link);
+echo 'SUHAIMI'
+?>
